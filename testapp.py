a7fac582a3027f14d6bc393863d9f26665379394
suraj3581 <surajstha3581@gmail.com>
2023-09-04 04:59:19 +0545

    First Python App.

---
+++ testapp.py
@@ -0,0 +1,8 @@
+def application(environ, start_response):
+    status= '200 Ok'
+    output = b'Hello World! This is test web app in python using CI/CD pipeline'
+    response_headers = [('Content-type','text/plain'),
+    ('Content-Length', str(len(output)))]
+    start_response(status,response_headers)
+    return [output]
+
