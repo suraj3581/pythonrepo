45c930e12f8aec16f8ac320e6b110d323eaafd3b
suraj3581 <surajstha3581@gmail.com>
2023-09-04 07:43:05 +0545

    Second version of python app

--- testapp.py
+++ testapp.py
@@ -1,6 +1,6 @@
 def application(environ, start_response):
     status= '200 Ok'
-    output = b'Hello World! This is test web app in python using CI/CD pipeline'
+    output = b'Hello World! This is test web app in python using CI/CD pipeline----modified version'
     response_headers = [('Content-type','text/plain'),
     ('Content-Length', str(len(output)))]
     start_response(status,response_headers)
